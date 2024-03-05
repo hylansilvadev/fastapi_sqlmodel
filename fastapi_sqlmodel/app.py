1057521f8573c748056c8b126e0650ea5c5d3e7d
Hylan Silva <hylansilva@outlook.com>
2024-03-05 13:04:06 -0300

    updatimg initial app

--- fastapi_sqlmodel/app.py
+++ fastapi_sqlmodel/app.py
@@ -1,19 +1,26 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
 from .core.settings import settings
 from .database.database import create_db_and_tables
 from .domains.event.event_route import events
 from .domains.user.user_route import users
 
-app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSSION)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    version=settings.PROJECT_VERSSION,
+    lifespan=lifespan,
+    docs_url="/docs"
+)
 
 app.include_router(events)
 app.include_router(users)
-
-
-@app.on_event('startup')
-def create_engine():
-    create_db_and_tables()
 
 
 @app.get('/', tags=['Default Route'])
